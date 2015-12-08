3c258731011eaea12b4b811544cbe401415e56e5
David Whitney <david@davidwhitney.co.uk>
2015-12-08 17:36:38 +0000

    Day 1 - challange 1.

---
+++ AdventOfCode/Dec1/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace AdventOfCode.Dec1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Climber_Process_GivenOneOpenBracket_CountsUp()
+        {
+            var climber = new Climber();
+
+            var floor = climber.Climb("(");
+
+            Assert.That(floor, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Climber_Process_GivenOneCloseBracket_CountsDown()
+        {
+            var climber = new Climber();
+
+            var floor = climber.Climb(")");
+
+            Assert.That(floor, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Climber_DoChallange()
+        {
+            var contents = File.ReadAllText("c:\\dev\\AdventOfCode\\AdventOfCode\\Dec1\\Test.txt");
+
+            var floor = new Climber().Climb(contents);
+
+            Console.WriteLine(floor);
+        }
+    }
+
+    public class Climber
+    {
+        public int Climb(string plan)
+        {
+            return plan.Count(x => x == '(') - plan.Count(x => x == ')');
+        }
+    }
+}
