a1f1ce1df958b8a006c5d8c3a585a2fd929b486d
David Whitney <david@davidwhitney.co.uk>
2015-12-08 23:22:52 +0000

    Mine part 1.

---
+++ AdventOfCode/Dec4/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode.Dec4
+{
+    [TestFixture]
+    public class Tests
+    {
+        private AdventCoinMiner _miner;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _miner = new AdventCoinMiner();
+        }
+
+        [Ignore("Slow!")]
+        [TestCase("abcdef", 609043)]
+        [TestCase("pqrstuv", 1048970)]
+        [TestCase("yzbqklnj", 282749)] // Challenge
+        public void Mine_WithKnownHash_ReturnsKnownValue(string stub, int expected)
+        {
+            var lowestStem = _miner.Mine(stub);
+
+            Assert.That(lowestStem, Is.EqualTo(expected));
+        }
+    }
+
+    public class AdventCoinMiner
+    {
+        public int Mine(string stub)
+        {
+            for (var i = 0; i < int.MaxValue; i++)
+            {
+                var hash = Hash(stub + i);
+
+                if (hash.StartsWith("00000"))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        private static string Hash(string stub)
+        {
+            var encodedPassword = new UTF8Encoding().GetBytes(stub);
+            var hash = ((HashAlgorithm) CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            return BitConverter.ToString(hash)
+                .Replace("-", string.Empty)
+                .ToLower();
+        }
+    }
+}
