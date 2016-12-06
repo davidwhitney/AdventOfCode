6109668184a12fe10e8c9015eda054b9d5f5ef05
David Whitney <david@davidwhitney.co.uk>
2016-12-06 19:18:38 +0000

    Phone pads.

--- 2016/AoC/Day2.cs
+++ 2016/AoC/Day2.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace AoC
@@ -14,9 +11,7 @@ namespace AoC
         [Test]
         public void Sample()
         {
-            var phonePad = new PhonePad();
-
-            var result = phonePad.Translate(new List<string>
+            var result = new PhonePad().Translate(new List<string>
             {
                 "ULL",
                 "RRDDD",
@@ -27,31 +22,61 @@ namespace AoC
             Assert.That(result, Is.EqualTo(1985));
         }
 
-        public class PhonePad : List<List<int>>
+        [Test]
+        public void Test()
         {
-            private int _fingerX = 1;
-            private int _fingerY = 1;
+            var contents = File.ReadAllLines("day2.txt");
+
+            var result = new PhonePad().Translate(contents);
+
+            Assert.That(result, Is.EqualTo(35749));
+        }
+
+        public class PhonePad : List<List<string>>
+        {
+            private readonly int[] _finger = {1, 1};
+            public string Current => this[_finger[1]][_finger[0]];
 
             public PhonePad()
             {
-                this.AddRange(new List<List<int>>
+                AddRange(new List<List<string>>
                 {
-                    new List<int> {1, 2, 3},
-                    new List<int> {4, 5, 6},
-                    new List<int> {7, 8, 9}
+                    new List<string> {"1", "2", "3"},
+                    new List<string> {"4", "5", "6"},
+                    new List<string> {"7", "8", "9"}
+                });
+
+
+                AddRange(new List<List<string>>
+                {
+                    new List<string> {"-", "-", "1", "-", "-"},
+                    new List<string> {"-", "2", "3", "4", "-"},
+                    new List<string> {"5", "6", "7", "8", "9"},
+                    new List<string> {"-", "A", "B", "C", "-"},
+                    new List<string> {"-", "-", "D", "-", "-"},
                 });
             }
 
-            public int Translate(List<string> sequence)
+            public int Translate(IEnumerable<string> sequence)
             {
+                var buffer = new List<string>();
                 foreach (var line in sequence)
                 {
-                    foreach (var move in line)
-                    {
-                    }
+                    line.ToList().ForEach(Process);
+                    buffer.Add(Current);
                 }
 
-                return -1;
+                var elements = string.Join("", buffer);
+                return int.Parse(elements);
+            }
+
+            private void Process(char move)
+            {
+                var delta = new Dictionary<char, int> {{'U', -1}, {'L', -1}, {'D', 1}, {'R', 1}}[move];
+                var axis = new Dictionary<char, int> {{'U', 1}, {'L', 0}, {'D', 1}, {'R', 0}}[move];
+                var preview = _finger[axis] + delta;
+                if (preview > 2 || preview < 0) return;
+                _finger[axis] += delta;
             }
         }
     }
