1176edc94920951daee552bd139b8884260c903a
David Whitney <david@davidwhitney.co.uk>
2016-12-06 14:53:35 +0000

    Cleaned up the "code golf-ey" solution to day 1.

--- 2016/AoC/Day1.cs
+++ 2016/AoC/Day1.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace AoC
@@ -10,104 +8,101 @@ namespace AoC
     [TestFixture]
     public class Day1
     {
-        private string _input =
-            "L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4";
+        private string _input = "L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4";
+        private IEnumerable<string> Instructions => _input.Split(',').ToList().Select(s => s.Trim());
+
+        private Pathfinder _pathfinder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pathfinder = new Pathfinder();
+        }
 
         [Test]
         public void Solve()
         {
-            var instructions = _input.Split(',').ToList().Select(s=>s.Trim());
-
-            var distance = Navigate(instructions);
-
-            Console.WriteLine(distance);
+            var distance = _pathfinder.Navigate(Instructions).Distance;
+            
             Assert.That(distance, Is.EqualTo(181));
         }
 
         [Test]
         public void Solve2()
         {
-            var instructions = _input.Split(',').ToList().Select(s=>s.Trim());
-            var history = new List<Location>();
+            _pathfinder = new Pathfinder(true);
 
-            Navigate(instructions, history);
-            var loc = GetFirstDupe(history);
-            Console.WriteLine("First double: " + loc + " at distance: " + loc.Distance );
+            var loc = _pathfinder.Navigate(Instructions);
+
+            Assert.That(loc.ToString(), Is.EqualTo("X:28,Y:112"));
+            Assert.That(loc.Distance, Is.EqualTo(140));
+        }
+    }
+
+    public class Pathfinder
+    {
+        private readonly bool _returnOnRevisit;
+        public List<Loc> History { get; } = new List<Loc>();
+
+        public Pathfinder(bool returnOnRevisit = false)
+        {
+            _returnOnRevisit = returnOnRevisit;
         }
 
-        private static Location GetFirstDupe(List<Location> history)
+        public Loc Navigate(IEnumerable<string> instructions)
         {
-            Location firstDupe = null;
-            var visited = new List<string>();
-            foreach (var item in history)
-            {
-                if (visited.Contains(item.ToString()))
-                {
-                    firstDupe = item;
-                    break;
-                }
-                visited.Add(item.ToString());
-            }
-            return firstDupe;
-        }
-
-        private static int Navigate(IEnumerable<string> instructions, List<Location> history = null)
-        {
-            history = history ?? new List<Location>();
-
             var baring = 0;
-            var coords = new[] {0, 0};
+            var coords = new Loc(0, 0);
             foreach (var instruction in instructions)
             {
-                var rotation = new Dictionary<char, int> {{'L', -1}, {'R', 1}}[instruction[0]];
+                var rotation = new Dictionary<char, int> { { 'L', -1 }, { 'R', 1 } }[instruction[0]];
                 baring = baring + rotation;
                 baring = baring > 3 ? 0 : baring;
                 baring = baring < 0 ? 3 : baring;
 
                 var distance = int.Parse(instruction.Trim('L', 'R'));
-                distance = baring > 1 ? distance*-1 : distance;
-                var index = baring%2 == 0 ? 1 : 0;
+                var directionalDistance = baring > 1 ? distance * -1 : distance;
+                var xOrY = baring % 2 == 0 ? 1 : 0;
 
-                RecordHistory(history, coords, baring, distance, index);
-                coords[index] += distance;
+                var detectedRevisit = RecordHistory(coords, baring, directionalDistance, xOrY);
+                coords[xOrY] += directionalDistance;
+
+                if (_returnOnRevisit && detectedRevisit != null)
+                {
+                    return detectedRevisit;
+                }
             }
 
-            return Math.Abs(coords[0]) + Math.Abs(coords[1]);
+            return Loc.Copy(coords);
         }
 
-        private static void RecordHistory(ICollection<Location> history, IReadOnlyList<int> coords, int baring, int distance, int index)
+        private Loc RecordHistory(IList<int> coords, int baring, int distance, int xOrY)
         {
-            var snapshot = new[] {coords[0], coords[1]};
+            var snapshot = Loc.Copy(coords);
             var distanceDelta = baring > 1 ? -1 : 1;
-            for (int i = 0; i < Math.Abs(distance); i++)
+            for (var i = 0; i < Math.Abs(distance); i++)
             {
-                snapshot[index] += distanceDelta;
-                history.Add(new Location(snapshot[0], snapshot[1]));
+                snapshot[xOrY] += distanceDelta;
+                var historyEntry = Loc.Copy(snapshot);
+                if (_returnOnRevisit && History.Any(x => x.ToString() == historyEntry.ToString()))
+                {
+                    return historyEntry;
+                }
+
+                History.Add(historyEntry);
             }
+
+            return null;
         }
+    }
 
-        public class Location
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-
-            public int Distance => Math.Abs(X) + Math.Abs(Y);
-
-            public Location(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
-
-            public override string ToString()
-            {
-                return $"X:{X},Y:{Y}";
-            }
-
-            public override bool Equals(object obj)
-            {
-                return ToString() == obj.ToString();
-            }
-        }
+    public class Loc : List<int>
+    {
+        public int X { get { return this[0]; } set { this[0] = value; } }
+        public int Y { get { return this[1]; } set { this[1] = value; } }
+        public int Distance => Math.Abs(X) + Math.Abs(Y);
+        public override string ToString() => $"X:{X},Y:{Y}";
+        public static Loc Copy(IList<int> x) => new Loc(x[0], x[1]);
+        public Loc(int x, int y) { AddRange(new[] { x, y }); }
     }
 }
