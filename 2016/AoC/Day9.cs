03929c67a3525570569f964aaaee95a66a09e003
David Whitney <david@davidwhitney.co.uk>
2016-12-12 02:13:07 +0000

    Bad tokeniser.

--- 2016/AoC/Day9.cs
+++ 2016/AoC/Day9.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AoC.Infrastructure;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace AoC
 {
@@ -38,6 +39,16 @@ namespace AoC
             var result = _compressor.Decompress(string.Join("", Input));
 
             Assert.That(result.Decompressed.Length, Is.EqualTo(74532));
+        }
+
+        [TestCase("XYZ", 3)]
+        [TestCase("(3x3)XYZ", 9)]
+        [TestCase("X(8x2)(3x3)ABCY", 20)]
+        public void Test2(string compressed, int length)
+        {
+            var lengthPrediction = _compressor.PredictLength2(compressed);
+
+            Assert.That(lengthPrediction, Is.EqualTo(length));
         }
     }
 
@@ -86,6 +97,98 @@ namespace AoC
             }
 
             return new Body(compressed, buffer.ToString());
+        }
+
+        public int PredictLength2(string compressed)
+        {
+            var inputTokens = compressed.Split(new[] {'(', ')'}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var exRg = new Regex(@"([0-9]+)x([0-9]+)");
+            var tokes = new List<Token>();
+
+            foreach (var input in inputTokens)
+            {
+                var thisToke = exRg.IsMatch(input)
+                    ? (Token) new ExpressionToken(input)
+                    : new LiteralToken(input);
+
+                var lastOrDefault = tokes.LastOrDefault();
+
+                if (lastOrDefault is ExpressionToken
+                    && thisToke is LiteralToken)
+                {
+                    var et = lastOrDefault as ExpressionToken;
+                    var t1 = new LiteralToken(input.Substring(0, et.SequenceLength));
+                    var t2 = new LiteralToken(input.Substring(et.SequenceLength, input.Length - et.SequenceLength));
+                    lastOrDefault.SetNext(t1);
+                    t1.SetNext(t2);
+                    tokes.Add(t1);
+                    tokes.Add(t2);
+                }
+                else
+                {
+                    lastOrDefault?.SetNext(thisToke);
+                    tokes.Add(thisToke);
+                }
+            }
+
+            int length = 0;
+            foreach (var token in tokes)
+            {
+                length += token.ComputedLength();
+            }
+            
+            return length;
+        }
+    }
+
+    public class ExpressionToken : Token
+    {
+        public ExpressionToken(string value) : base(value)
+        {
+            var expanderRegex = new Regex(@"([0-9]+)x([0-9]+)");
+            if (expanderRegex.IsMatch(value))
+            {
+                var matches = expanderRegex.Matches(value)[0];
+                SequenceLength = int.Parse(matches.Groups[1].Value);
+                PrintDuration = int.Parse(matches.Groups[2].Value);
+            }
+        }
+
+        public int SequenceLength { get; }
+        public int PrintDuration { get; }
+
+        public override int ComputedLength() => ComputeLength();
+
+        private int ComputeLength()
+        {
+            return Next.ComputedLength() * (PrintDuration - 1);
+        }
+    }
+
+
+    public class LiteralToken : Token
+    {
+        public LiteralToken(string value) : base(value)
+        {
+        }
+
+        public override int ComputedLength() => Value.Length;
+    }
+
+    public abstract class Token
+    {
+        public Token Next { get; set; }
+        public string Value { get; set; }
+        public abstract int ComputedLength();
+
+        protected Token(string value)
+        {
+            Value = value;
+        }
+
+        public void SetNext(Token t)
+        {
+            Next = t;
         }
     }
 
