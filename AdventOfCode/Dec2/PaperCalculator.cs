4b4c2b59918c22c9f052925393db66a6e9239c48
David Whitney <david@davidwhitney.co.uk>
2017-12-03 14:59:22 +0000

    ??

--- AdventOfCode/Dec2/PaperCalculator.cs
+++ AdventOfCode/Dec2/PaperCalculator.cs
@@ -1,10 +1,40 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace AdventOfCode.Dec2
 {
     public class PaperCalculator
     {
+        public PaperCalculator()
+        {
+            var data = "data";
+            var result = data is string a ? a : "Not a String";
+
+            void TotesALocalFunction()
+            {
+                Debug.WriteLine("This works too");
+            }
+
+            TotesALocalFunction();
+
+            var parsed = int.TryParse("1", out var numberParsed);
+
+            var ints = GetInts();
+            var aa = ints.a;
+            var ab = ints.b;
+
+            (int a, int b) GetInts()
+            {
+                // How about tuples!
+                return (1, 2);
+            }
+
+            var ccc = (A: "a", B: "aa");
+            var aaaaa = ccc.A;
+        }
+
         public int SurfaceAreaOf(int length, int width, int height)
         {
             return 2*length*width + 2*width*height + 2*height*length;
@@ -30,6 +60,14 @@ namespace AdventOfCode.Dec2
 
         public int RibbonRequred(params int[] sides)
         {
+            int.TryParse("1", out var abc);
+            Console.WriteLine(abc);
+
+            void A()
+            {
+                
+            }
+
             var sidesOrdered = sides.OrderBy(x => x).ToList();
             var subTotal = sidesOrdered[0] + sidesOrdered[0] + sidesOrdered[1] + sidesOrdered[1];
             var ribbonLength = sidesOrdered[0]*sidesOrdered[1]*sidesOrdered[2];
