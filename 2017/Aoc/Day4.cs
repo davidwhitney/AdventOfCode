dafd906df58706240ff30bd0baac168fd9d9b6d5
David Whitney <david@davidwhitney.co.uk>
2017-12-07 15:42:10 +0000

    Part 2.

--- 2017/Aoc/Day4.cs
+++ 2017/Aoc/Day4.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace Aoc
 {
@@ -22,7 +19,18 @@ namespace Aoc
             Console.WriteLine(valid);
         }
 
-        private static bool IsValid(string line)
+        [Test]
+        public void Part2()
+        {
+            var allData = File.ReadAllLines(@"C:\dev\AdventOfCode\2017\Aoc\Day4-1.txt");
+
+            var valid = allData.Count(l => IsValid(l, true));
+
+            Console.WriteLine(valid);
+        }
+
+        private static bool IsValid(string line) => IsValid(line, false);
+        private static bool IsValid(string line, bool normaliseAnagrams)
         {
             var allTokens = new Queue<string>(line.Split(' '));
             var seen = new List<string>();
@@ -30,6 +38,12 @@ namespace Aoc
             while (allTokens.Count > 0)
             {
                 var current = allTokens.Dequeue();
+
+                if (normaliseAnagrams)
+                {
+                    current = string.Join("", current.OrderBy(c => c));
+                }
+
                 if (!seen.Contains(current))
                 {
                     seen.Add(current);
